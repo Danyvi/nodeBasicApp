b0f5aa013c742739f7f0b3a9936f40e7e6ac428c
Daniele Vinci <vinci.dani@gmail.com>
2019-04-12 12:51:17 -0400

    response output formatted

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const username = 'chalkers';
 // Function to print message to console
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-  console.log('Logged Output: printMessage -> message', message);
+  console.log(message);
 }
 
 // Connect to the API URL (https://teamtreehouse.com/username.json)
@@ -18,9 +18,25 @@ const request = https.get(
   `https://teamtreehouse.com/${username}.json`,
   response => {
     console.log('statusCode: ', response.statusCode);
-    console.log('headers: ', response.headers);
+    // console.log('headers: ', response.headers);
+    let responseBody = '';
     // Read the data (it will read the data as a string)
-    // Parse the data (since it JSON we'll need to pass that data)
-    // Print out the information we obtained from the API
+    response.on('data', dataChunk => {
+      responseBody += dataChunk.toString();
+    });
+
+    response.on('end', () => {
+      // Parse the data (since it JSON we'll need to parse that data)
+      // (process of converting a string into a data structure is called parsing)
+      // JSON.parse(text), we pass a string to get parsed into a JSON object
+      // we'll store the object as a const variable called profile
+      const profile = JSON.parse(responseBody);
+      //console.dir(profile);
+
+      // console.log(responseBody);
+      // console.log(typeof responseBody);
+      // Print out the information we obtained from the API
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+    });
   }
 );
