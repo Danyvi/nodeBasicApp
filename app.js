784ec4a3cf5bfeca601497ba87186c4df81de72e
Daniele Vinci <vinci.dani@gmail.com>
2019-04-12 13:11:49 -0400

    added users array

--- app.js
+++ app.js
@@ -4,39 +4,54 @@
 // Require https module (To require the https module in our code we need to use the require function)
 const https = require('https');
 
-// let define the username
-const username = 'chalkers';
-
 // Function to print message to console
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
   console.log(message);
 }
 
-// Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(
-  `https://teamtreehouse.com/${username}.json`,
-  response => {
-    console.log('statusCode: ', response.statusCode);
-    // console.log('headers: ', response.headers);
-    let responseBody = '';
-    // Read the data (it will read the data as a string)
-    response.on('data', dataChunk => {
-      responseBody += dataChunk.toString();
-    });
+function getProfile(username) {
+  // Connect to the API URL (https://teamtreehouse.com/username.json)
+  const request = https.get(
+    `https://teamtreehouse.com/${username}.json`,
+    response => {
+      // console.log('statusCode: ', response.statusCode);
+      // console.log('headers: ', response.headers);
+      let responseBody = '';
+      // Read the data (it will read the data as a string)
+      response.on('data', dataChunk => {
+        responseBody += dataChunk.toString();
+      });
 
-    response.on('end', () => {
-      // Parse the data (since it JSON we'll need to parse that data)
-      // (process of converting a string into a data structure is called parsing)
-      // JSON.parse(text), we pass a string to get parsed into a JSON object
-      // we'll store the object as a const variable called profile
-      const profile = JSON.parse(responseBody);
-      //console.dir(profile);
+      response.on('end', () => {
+        // Parse the data (since it JSON we'll need to parse that data)
+        // (process of converting a string into a data structure is called parsing)
+        // JSON.parse(text), we pass a string to get parsed into a JSON object
+        const profile = JSON.parse(responseBody);
+        //console.dir(profile);
+        // console.log(responseBody);
+        // console.log(typeof responseBody);
+        // Print out the information we obtained from the API
+        printMessage(
+          username,
+          profile.badges.length,
+          profile.points.JavaScript
+        );
+      });
+    }
+  );
+}
 
-      // console.log(responseBody);
-      // console.log(typeof responseBody);
-      // Print out the information we obtained from the API
-      printMessage(username, profile.badges.length, profile.points.JavaScript);
-    });
-  }
-);
+// process; it is the corrispondent of window in JS in the browser
+// console.dir(process.argv);
+
+// const users = ['chalkers', 'alenaholligan', 'davemcfarland'];
+const users = process.argv.slice(2);
+
+// Expanded version of the forEach loop
+// users.forEach(username => {
+//   getProfile(username);
+// });
+
+// shorter version
+users.forEach(getProfile);
